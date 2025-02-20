a772d815864944fb2a0cacd15c9fc306d956e057
Takahiro Nagasawa <takahiro.nagasawa@proton.me>
2025-02-20 16:54:30 +0100

    Add support chat export API endpoint
    Signed-off-by: Takahiro Nagasawa <takahiro.nagasawa@proton.me>

--- http-api/src/main/java/bisq/http_api/web_socket/WebSocketRestApiResourceConfig.java
+++ http-api/src/main/java/bisq/http_api/web_socket/WebSocketRestApiResourceConfig.java
@@ -6,6 +6,7 @@ import bisq.http_api.rest_api.domain.market_price.MarketPriceRestApi;
 import bisq.http_api.rest_api.domain.offers.OfferbookRestApi;
 import bisq.http_api.rest_api.domain.payment_accounts.PaymentAccountsRestApi;
 import bisq.http_api.rest_api.domain.settings.SettingsRestApi;
+import bisq.http_api.rest_api.domain.support.SupportRestApi;
 import bisq.http_api.rest_api.domain.trades.TradeRestApi;
 import bisq.http_api.rest_api.domain.user_identity.UserIdentityRestApi;
 import jakarta.ws.rs.ApplicationPath;
@@ -21,7 +22,8 @@ public class WebSocketRestApiResourceConfig extends RestApiResourceConfig {
                                           MarketPriceRestApi marketPriceRestApi,
                                           SettingsRestApi settingsRestApi,
                                           ExplorerRestApi explorerRestApi,
-                                          PaymentAccountsRestApi paymentAccountsRestApi) {
-        super(swaggerBaseUrl, offerbookRestApi, tradeRestApi, userIdentityRestApi, marketPriceRestApi, settingsRestApi, explorerRestApi, paymentAccountsRestApi);
+                                          PaymentAccountsRestApi paymentAccountsRestApi,
+                                          SupportRestApi supportRestApi) {
+        super(swaggerBaseUrl, offerbookRestApi, tradeRestApi, userIdentityRestApi, marketPriceRestApi, settingsRestApi, explorerRestApi, paymentAccountsRestApi, supportRestApi);
     }
 }
